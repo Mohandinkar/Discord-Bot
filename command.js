fb4106de3b561343834d1651f7f9cab0b2e5126a
Mohandinkar <mohandinkar75@gmail.com>
2025-02-25 15:40:16 +0530

    added

--- command.js
+++ command.js
@@ -1,38 +1,39 @@
-const { REST, Routes, Options } = require('discord.js');
-require('dotenv').config();
-const TOKEN = process.env.RESET_TOKEN; 
+//command registration 
+
+const { REST, Routes } = require("discord.js");
+require("dotenv").config();
+const TOKEN = process.env.RESET_TOKEN;
 const CLIENT = process.env.CLIENT_ID;
 
 const commands = [
   {
-    name: 'ping',
-    description: 'Replies with Pong!',
+    name: "ping",
+    description: "Replies with Pong!",
   },
   {
-    name:'chat',
-    description: 'Chat with the bot!',
-    options:[
+    name: "chat",
+    description: "Chat with the bot!",
+    options: [
       {
-        name:'user',
-        type :3,//msg is a type of string
-        description: 'Msg will be send to BOT!',
+        name: "user",
+        type: 3, //msg is a type of string
+        description: "Msg will be send to BOT!",
         required: true,
       },
-    ]
-
+    ],
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
-(async()=>{
-    try {
-        console.log('Started refreshing application (/) commands.');
-      
-        await rest.put(Routes.applicationCommands(CLIENT), { body: commands });
-      
-        console.log('Successfully reloaded application (/) commands.');
-      } catch (error) {
-        console.error(error);
-      }
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationCommands(CLIENT), { body: commands });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
 })();
